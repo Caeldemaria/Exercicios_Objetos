da453645209ba28f6f4d990d224aef404d619b8b
Caeldemaria <138541178+Caeldemaria@users.noreply.github.com>
2023-07-28 09:25:13 -0300

    final

---
+++ 06/index.js
@@ -0,0 +1,35 @@
+const usuarios = [
+    {
+        nome: "João",
+        idade: 25,
+    },
+    {
+        nome: "Ana",
+        idade: 18,
+    },
+    {
+        nome: "Beatriz",
+        idade: 15,
+    },
+    {
+        nome: "Carlos",
+        idade: 16,
+    },
+    {
+        nome: "Antonio",
+        idade: 32,
+    },
+]
+let jovens = []
+let adultos = []
+for (let i = 0; i < usuarios.length; i++) {
+    if (usuarios[i].idade >= 18) {
+        adultos.push(usuarios[i])
+
+    }
+    else if (usuarios[i].idade < 18) {
+        jovens.push(usuarios[i])
+    }
+} console.log(adultos)
+console.log(jovens)
+
