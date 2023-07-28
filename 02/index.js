e62e455b0ca29225694b1b6b2363a2ba97cbd47c
Caeldemaria <138541178+Caeldemaria@users.noreply.github.com>
2023-07-27 21:15:32 -0300

    final

---
+++ 02/index.js
@@ -0,0 +1,23 @@
+const carro1 = {
+    marca: "fiat",
+    modelo: "Mobi",
+    ano: 2022,
+    cor: "branco",
+    quantidade_portas: 4,
+    automatico: false
+};
+const carro2 = {
+    marca: "Honda",
+    modelo: "civic",
+    ano: 2022,
+    cor: "Azul",
+    quantidade_portas: 4,
+    automatico: true
+}; const carro3 = {
+    marca: "Chevrolet",
+    modelo: "Camaro",
+    ano: 2024,
+    cor: "Amarelo",
+    quantidade_portas: 4,
+    automatico: true
+}
