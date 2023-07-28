086eac308ea86aa1240856f59af8ebbc28576cb6
Caeldemaria <138541178+Caeldemaria@users.noreply.github.com>
2023-07-27 21:03:51 -0300

    final

---
+++ 01/index.js
@@ -0,0 +1,19 @@
+const endereco = {
+    rua: 'B',
+    numero: 74,
+    complmento: "a",
+    bairro: 'Barrocão',
+    cep: 61880000,
+    cidade: 'Itaitinga',
+    estado: 'Ceará',
+    pais: 'Brasil'
+}
+const usuario = {
+    noeme: 'José',
+    elmail: 'jose.l@gmail.com',
+    telefone: 879554644,
+    data_nascimento: '30/07/199',
+    endereco: endereco,
+
+}
+console.log(usuario)
